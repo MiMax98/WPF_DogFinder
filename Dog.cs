98ba4c995fd1ff69130f3e5ace8dd3f28f5b9579
Michał Witczak <michalwitczak19@gmail.com>
2021-05-30 13:39:14 +0200

    Visual changes

--- Dog.cs
+++ Dog.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RasyPsow
 {
@@ -8,6 +9,63 @@ namespace RasyPsow
         public DogSize Size { get; set; }
         public bool FirstDog { get; set; }
         public bool Active { get; set; }
+
+        public static List<Dog> GetDogs()
+        {
+            return new List<Dog>
+            {
+                new Dog { Name = "Aidi", Size=DogSize.Medium, FirstDog=false, Active=false},
+                new Dog { Name = "Akita Inu", Size=DogSize.Large, FirstDog=false, Active=false},
+                new Dog { Name = "Ariegeois", Size=DogSize.Medium, FirstDog=true, Active=false},
+                new Dog { Name = "Barbet", Size=DogSize.Medium, FirstDog=true, Active=false},
+                new Dog { Name = "Basenji", Size=DogSize.Small, FirstDog=true, Active=false},
+                new Dog { Name = "Beagle", Size=DogSize.Medium, FirstDog=true, Active=false},
+                new Dog { Name = "Bernardyn", Size=DogSize.Large, FirstDog=true, Active=false},
+                new Dog { Name = "Billy", Size=DogSize.Large, FirstDog=true, Active=true},
+                new Dog { Name = "Boerboel", Size=DogSize.Large, FirstDog=false, Active=false},
+                new Dog { Name = "Bokser", Size=DogSize.Medium, FirstDog=true, Active=true},
+                new Dog { Name = "Bolończyk", Size=DogSize.Small, FirstDog=true, Active=false},
+                new Dog { Name = "Broholmer", Size=DogSize.Large, FirstDog=true, Active=false},
+                new Dog { Name = "Bulterier", Size=DogSize.Medium, FirstDog=false, Active=true},
+                new Dog { Name = "Chihuahua", Size=DogSize.Small, FirstDog=true, Active=false},
+                new Dog { Name = "Chow chow", Size=DogSize.Medium, FirstDog=false, Active=false},
+                new Dog { Name = "Cursinu", Size=DogSize.Large, FirstDog=true, Active=false},
+                new Dog { Name = "Doberman", Size=DogSize.Large, FirstDog=true, Active=true},
+                new Dog { Name = "Drever", Size=DogSize.Small, FirstDog=true, Active=true},
+                new Dog { Name = "Dunker", Size=DogSize.Medium, FirstDog=true, Active=false},
+                new Dog { Name = "Eurasier", Size=DogSize.Medium, FirstDog=true, Active=false},
+                new Dog { Name = "Greyhound", Size=DogSize.Large, FirstDog=false, Active=false},
+                new Dog { Name = "Harrier", Size=DogSize.Medium, FirstDog=true, Active=false},
+                new Dog { Name = "Hawańczyk", Size=DogSize.Small, FirstDog=true, Active=false},
+                new Dog { Name = "Hokkaido", Size=DogSize.Medium, FirstDog=false, Active=false},
+                new Dog { Name = "Hovawart", Size=DogSize.Large, FirstDog=true, Active=false},
+                new Dog { Name = "Hygenhund", Size=DogSize.Medium, FirstDog=true, Active=false},
+                new Dog { Name = "Jamnik", Size=DogSize.Small, FirstDog=true, Active=false},
+                new Dog { Name = "Jämthund", Size=DogSize.Large, FirstDog=false, Active=false},
+                new Dog { Name = "Jindo", Size=DogSize.Medium, FirstDog=false, Active=false},
+                new Dog { Name = "Kai", Size=DogSize.Medium, FirstDog=true, Active=false},
+                new Dog { Name = "Kelpie", Size=DogSize.Medium, FirstDog=true, Active=true},
+                new Dog { Name = "Kishu", Size=DogSize.Medium, FirstDog=true, Active=false},
+                new Dog { Name = "Komondor", Size=DogSize.Large, FirstDog=false, Active=false},
+                new Dog { Name = "Kuvasz", Size=DogSize.Large, FirstDog=false, Active=false},
+                new Dog { Name = "Landseer", Size=DogSize.Large, FirstDog=false, Active=false},
+                new Dog { Name = "Mops", Size=DogSize.Small, FirstDog=true, Active=false},
+                new Dog { Name = "Mudi", Size=DogSize.Medium, FirstDog=true, Active=true},
+                new Dog { Name = "Pekińczyk", Size=DogSize.Small, FirstDog=true, Active=false},
+                new Dog { Name = "Pointer", Size=DogSize.Medium, FirstDog=true, Active=false},
+                new Dog { Name = "Poitevin", Size=DogSize.Large, FirstDog=true, Active=false},
+                new Dog { Name = "Pudel", Size=DogSize.Medium, FirstDog=true, Active=true},
+                new Dog { Name = "Puli", Size=DogSize.Medium, FirstDog=true, Active=false},
+                new Dog { Name = "Pumi", Size=DogSize.Medium, FirstDog=true, Active=true},
+                new Dog { Name = "Shar pei", Size=DogSize.Medium, FirstDog=false, Active=false},
+                new Dog { Name = "Shiba inu", Size=DogSize.Small, FirstDog=true, Active=true},
+                new Dog { Name = "Shih tzu", Size=DogSize.Small, FirstDog=true, Active=false},
+                new Dog { Name = "Shikoku", Size=DogSize.Medium, FirstDog=false, Active=false},
+                new Dog { Name = "Tornjak", Size=DogSize.Large, FirstDog=true, Active=false},
+                new Dog { Name = "Tosa", Size=DogSize.Large, FirstDog=false, Active=false},
+                new Dog { Name = "Whippet", Size=DogSize.Medium, FirstDog=true, Active=false},
+            };
+        }
     }
 
     [Flags]
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RasyPsow
 {
@@ -29,60 +20,7 @@ namespace RasyPsow
         public MainWindow()
         {
             _sizes = DogSize.Undefined;
-            _dogs = new List<Dog>
-            {
-                new Dog { Name = "Aidi", Size=DogSize.Medium, FirstDog=false, Active=false},
-                new Dog { Name = "Akita Inu", Size=DogSize.Large, FirstDog=false, Active=false},
-                new Dog { Name = "Ariegeois", Size=DogSize.Medium, FirstDog=true, Active=false},
-                new Dog { Name = "Barbet", Size=DogSize.Medium, FirstDog=true, Active=false},
-                new Dog { Name = "Basenji", Size=DogSize.Small, FirstDog=true, Active=false},
-                new Dog { Name = "Beagle", Size=DogSize.Medium, FirstDog=true, Active=false},
-                new Dog { Name = "Bernardyn", Size=DogSize.Large, FirstDog=true, Active=false},
-                new Dog { Name = "Billy", Size=DogSize.Large, FirstDog=true, Active=true},
-                new Dog { Name = "Boerboel", Size=DogSize.Large, FirstDog=false, Active=false},
-                new Dog { Name = "Bokser", Size=DogSize.Medium, FirstDog=true, Active=true},
-                new Dog { Name = "Bolończyk", Size=DogSize.Small, FirstDog=true, Active=false},
-                new Dog { Name = "Broholmer", Size=DogSize.Large, FirstDog=true, Active=false},
-                new Dog { Name = "Bulterier", Size=DogSize.Medium, FirstDog=false, Active=true},
-                new Dog { Name = "Chihuahua", Size=DogSize.Small, FirstDog=true, Active=false},
-                new Dog { Name = "Chow chow", Size=DogSize.Medium, FirstDog=false, Active=false},
-                new Dog { Name = "Cursinu", Size=DogSize.Large, FirstDog=true, Active=false},
-                new Dog { Name = "Doberman", Size=DogSize.Large, FirstDog=true, Active=true},
-                new Dog { Name = "Drever", Size=DogSize.Small, FirstDog=true, Active=true},
-                new Dog { Name = "Dunker", Size=DogSize.Medium, FirstDog=true, Active=false},
-                new Dog { Name = "Eurasier", Size=DogSize.Medium, FirstDog=true, Active=false},
-                new Dog { Name = "Greyhound", Size=DogSize.Large, FirstDog=false, Active=false},
-                new Dog { Name = "Harrier", Size=DogSize.Medium, FirstDog=true, Active=false},
-                new Dog { Name = "Hawańczyk", Size=DogSize.Small, FirstDog=true, Active=false},
-                new Dog { Name = "Hokkaido", Size=DogSize.Medium, FirstDog=false, Active=false},
-                new Dog { Name = "Hovawart", Size=DogSize.Large, FirstDog=true, Active=false},
-                new Dog { Name = "Hygenhund", Size=DogSize.Medium, FirstDog=true, Active=false},
-                new Dog { Name = "Jamnik", Size=DogSize.Small, FirstDog=true, Active=false},
-                new Dog { Name = "Jämthund", Size=DogSize.Large, FirstDog=false, Active=false},
-                new Dog { Name = "Jindo", Size=DogSize.Medium, FirstDog=false, Active=false},
-                new Dog { Name = "Kai", Size=DogSize.Medium, FirstDog=true, Active=false},
-                new Dog { Name = "Kelpie", Size=DogSize.Medium, FirstDog=true, Active=true},
-                new Dog { Name = "Kishu", Size=DogSize.Medium, FirstDog=true, Active=false},
-                new Dog { Name = "Komondor", Size=DogSize.Large, FirstDog=false, Active=false},
-                new Dog { Name = "Kuvasz", Size=DogSize.Large, FirstDog=false, Active=false},
-                new Dog { Name = "Landseer", Size=DogSize.Large, FirstDog=false, Active=false},
-                new Dog { Name = "Mops", Size=DogSize.Small, FirstDog=true, Active=false},
-                new Dog { Name = "Mudi", Size=DogSize.Medium, FirstDog=true, Active=true},
-                new Dog { Name = "Pekińczyk", Size=DogSize.Small, FirstDog=true, Active=false},
-                new Dog { Name = "Pointer", Size=DogSize.Medium, FirstDog=true, Active=false},
-                new Dog { Name = "Poitevin", Size=DogSize.Large, FirstDog=true, Active=false},
-                new Dog { Name = "Pudel", Size=DogSize.Medium, FirstDog=true, Active=true},
-                new Dog { Name = "Puli", Size=DogSize.Medium, FirstDog=true, Active=false},
-                new Dog { Name = "Pumi", Size=DogSize.Medium, FirstDog=true, Active=true},
-                new Dog { Name = "Shar pei", Size=DogSize.Medium, FirstDog=false, Active=false},
-                new Dog { Name = "Shiba inu", Size=DogSize.Small, FirstDog=true, Active=true},
-                new Dog { Name = "Shih tzu", Size=DogSize.Small, FirstDog=true, Active=false},
-                new Dog { Name = "Shikoku", Size=DogSize.Medium, FirstDog=false, Active=false},
-                new Dog { Name = "Tornjak", Size=DogSize.Large, FirstDog=true, Active=false},
-                new Dog { Name = "Tosa", Size=DogSize.Large, FirstDog=false, Active=false},
-                new Dog { Name = "Whippet", Size=DogSize.Medium, FirstDog=true, Active=false},
-
-            };
+            _dogs = Dog.GetDogs();
             InitializeComponent();
         }
 
@@ -140,7 +78,7 @@ namespace RasyPsow
 
             if (!string.IsNullOrEmpty(filter))
             {
-                result = result.Where(d => d.Name.Contains(filter));
+                result = result.Where(d => d.Name.Contains(filter, StringComparison.OrdinalIgnoreCase));
             }
             Result_list.Items.Clear();
             foreach (var item in result)
